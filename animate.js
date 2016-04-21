66f1a1f7b50cbfed2fc8a83d2a83ac99dde50ec2
Chloe Kaubisch <nemononiam@gmail.com>
2016-04-21 09:55:36 -0400

    look at all the problems

--- animate.js
+++ animate.js
@@ -3,71 +3,44 @@
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
 
-var start = document.getElementById("start");
-var stop  = document.getElementById("stop");
-var dvd   = document.getElementById("dvd");
-
-var r = 1;
-var growing = true;
 var requestID;
 
 var clr = function clr() {
     ctx.clearRect(0,0,538,538); // clears everything in the canvas
 };
 
-var anime = function anime(e) {
-    clr();
-    ctx.fillStyle = "red";
-    ctx.beginPath(); // new path
-    ctx.arc(269,269,r,0,2*Math.PI); // circle at center
-    ctx.fill(); // draws the circle
-    if (growing) {
-	r++;
-    } else {
-        r--;
+var coors = [];
+
+var move = function move() {
+    var logo = new Image();
+    logo.src = "logo_dvd.jpg";
+
+    if (this.x==0 || this.x+(logo.width/4)==538) {
+        this.dx*=-1;
     }
-    if (r <= 0) {
-	growing = true;
-    } else if (r > 269) {
-        growing = false;
+    if (this.y==0 || this.y+(logo.height/4)==538) {
+        this.dy*=-1;
     }
-    requestID = window.requestAnimationFrame(anime); // repeat
+    this.x+=this.dx;
+    this.y+=this.dy;
+    ctx.clearRect(0,0,538,538);
+    ctx.drawImage(logo,this.x,this.y,logo.width/4,logo.height/4);
+    requestID = window.requestAnimationFrame(move); // repeat                   
+}
+
+var add = function add(){
+    coors.push(turbo);
+    console.log(coors[coors.length -1]);
+    coors[coors.length - 1].bounce;
 };
 
-var no = function no() {
-    //console.log(requestID);
-    window.cancelAnimationFrame(requestID);
-}
+var turbo = {
+    dx: 1,
+    dy: 1,
+    x: Math.floor(Math.random() * 538),
+    y: Math.floor(Math.random() * 538),
+    bounce: move,
+    //move: screensave,
+};
 
-var screensave = function screensave() {
-    window.cancelAnimationFrame( requestID ); // stops another already running process
-
-    // var inits so pressing the button actually restarts it instead of speeding up
-    var x = 100;
-    var y = 100;
-    var dx = 1;
-    var dy = 1;
-
-    var move = function move() {
-        var logo = new Image();
-        logo.src = "logo_dvd.jpg";
-
-        if (x==0 || x+(logo.width/4)==538) {
-            dx*=-1;
-        }
-        if (y==0 || y+(logo.height/4)==538) {
-            dy*=-1;
-        }
-        x+=dx;
-        y+=dy;
-        ctx.clearRect(0,0,538,538);
-        ctx.drawImage(logo,x,y,logo.width/4,logo.height/4);
-        requestID = window.requestAnimationFrame(move); // repeat
-    }
-
-    move();
-}
-
-start.addEventListener("click", anime); // button triggers animation
-stop.addEventListener("click", no); // button stops animation
-dvd.addEventListener("click", screensave); // button dvds animation
+dvd.addEventListener("click", add); // button dvds animation
