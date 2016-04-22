7383f87788ab311d00cf6bd02a8e8d5c8dbc6ce0
kagers <Ka.gershfeld@gmail.com>
2016-04-21 23:33:24 -0400

    yes?

--- animate.js
+++ animate.js
@@ -1,49 +1,47 @@
-// Katherine Gershfeld
-
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
 
-var requestID;
-
-var clr = function clr() {
-    ctx.clearRect(0,0,538,538); // clears everything in the canvas
-};
-
 var coors = [];
-
-var move = function move() {
-    var logo = new Image();
-    logo.src = "logo_dvd.jpg";
-
-    if (this.x==0 || this.x+(logo.width/4)==538) {
-        this.dx*=-1;
-    }
-    if (this.y==0 || this.y+(logo.height/4)==538) {
-        this.dy*=-1;
-    }
-    this.x+=this.dx;
-    this.y+=this.dy;
-    ctx.clearRect(0,0,538,538);
-    ctx.drawImage(logo,this.x,this.y,logo.width/4,logo.height/4);
-    requestID = window.requestAnimationFrame(move); // repeat                   
-}
 
 var add = function add(){
     coors.push(turbo());
-    console.log(coors[coors.length -1]);
-    coors[coors.length - 1].bounce;
 };
 
 var turbo = function(){
+    var logo = new Image();
+    logo.src = "logo_dvd.jpg";
+    var move = function move() {
+	ctx.clearRect(this.x, this.y, logo.width/6, logo.height/6);
+   	if (this.x==0 || Math.floor(this.x+(logo.width/6))==538) {
+        	this.dx*=-1;
+    	}
+    	if (this.y==0 || Math.floor(this.y+(logo.height/6))==538) {
+        	this.dy*=-1;
+    	}
+    	this.x+=this.dx;
+    	this.y+=this.dy;
+    	ctx.drawImage(logo,this.x,this.y,logo.width/6,logo.height/6);               
+    };	
     var t = {
 	dx: 1,
 	dy: 1,
-	x: Math.floor(Math.random() * 538),
-	y: Math.floor(Math.random() * 538),
-	bounce: move,
-	//move: screensave,
+	x: Math.floor(Math.random() * (520-logo.width/6)-15)+15,
+	y: Math.floor(Math.random() * (520-logo.height/6)-15)+15,
+	move: move
     };
     return t;
 };
+
+var bounce = function() {
+    var animate = function() {
+	for (var i=0; i<coors.length; i++) {
+	    coors[i].move();
+	}
+	id=window.requestAnimationFrame(animate);
+    }
+    var id=window.requestAnimationFrame(animate);
+}
+
+bounce();
 
 dvd.addEventListener("click", add); // button dvds animation
